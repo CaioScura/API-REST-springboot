d4ecc41a5071003cfb652c8360e96899d7391639
CaioScura <caioscura02@gmail.com>
2026-06-24 23:09:29 -0300

    add classe e record(DTO) de pacientes

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,22 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import med.voll.paciente.DadosCadastroPaciente;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+        System.out.println(dados);
+    }
+    
+
+}
