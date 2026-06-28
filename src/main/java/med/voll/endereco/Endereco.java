c6e055e310955fbc0c90eab51febc168e5d8c709
CaioScura <caioscura02@gmail.com>
2026-06-28 18:59:09 -0300

    add entidades jpa medico

---
+++ src/main/java/med/voll/endereco/Endereco.java
@@ -0,0 +1,26 @@
+package med.voll.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Endereco
+ */
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+}
