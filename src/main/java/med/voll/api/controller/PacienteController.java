260c17c6686a50834f5b5cba967881562fa6aa8c
CaioScura <caioscura02@gmail.com>
2026-06-29 18:12:00 -0300

    cadastro de pacientes entidade, repository, migration

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,8 +3,13 @@ package med.voll.api.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,9 +18,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+
+    private final PacienteRepository repository;
+
+    PacienteController(PacienteRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
     
 
