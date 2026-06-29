0274369a455a0accebef9a4db04652dc43c8703f
CaioScura <caioscura02@gmail.com>
2026-06-29 17:02:51 -0300

    add migration com flyway

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,11 @@
 package med.voll.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import med.voll.medico.DadosCadastroMedico;
 import med.voll.medico.Medico;
 import med.voll.medico.MedicoRepository;
@@ -14,12 +14,17 @@ import med.voll.medico.MedicoRepository;
 @RequestMapping("medicos")
 public class MedicoController {
 
-    // injecao de ddependencia, o spring vai criar uma instancia do MedicoRepository e injetar aqui
-    @Autowired
-    private MedicoRepository repository;
+    // injecao por construtor (boa pratica recomendada pelo spring, substitui o @Autowired no campo)
+    // antes: @Autowired private MedicoRepository repository;
+    private final MedicoRepository repository;
+
+    public MedicoController(MedicoRepository repository) {
+        this.repository = repository;
+    }
 
     // requestbody no parametro serve para pegar o corpo da requisição e transformar em um objeto java, no caso aqui é uma string
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         // System.out.println(dados);
 
